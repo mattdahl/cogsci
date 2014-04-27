3de3bb712e2d65aedb9ba326c30a57c432e96ec1
Matt Dahl <matt.dahl.2013@gmail.com>
2014-04-27 01:26:20 -0700

    Fixes bug related to duplicate first card appearing.

--- cogsci.js
+++ cogsci.js
@@ -7,7 +7,7 @@ if (Meteor.isClient) {
 	var timer = null;
 
 	var present_next_card = function (event) {
-		if (current_card_index === 12 && current_state === 0) {
+		if (current_card_index === 11 && current_state === 0) {
 			current_card.toggleClass('current');
 			current_card_index = 0;
 			cards = _.shuffle($('.card'));
@@ -20,7 +20,7 @@ if (Meteor.isClient) {
 
 			current_state = 1;
 		}
-		else if (current_card_index === 12 && current_state === 1) {
+		else if (current_card_index === 11 && current_state === 1) {
 			current_audio.pause();
 			$('#instructions').html('Thanks! All finished.');
 		}
@@ -45,9 +45,8 @@ if (Meteor.isClient) {
 
 		Responses.insert(response);
 
-		debugger;
 		current_card.toggleClass('current'); // Hide old current_card
-		current_card = $(cards[current_card_index++]); // Advance to the new current_card
+		current_card = $(cards[++current_card_index]); // Advance to the new current_card
 		current_card.toggleClass('current'); // Present the new current_card
 
 		timer = new Date(); // Reset timer
@@ -65,6 +64,7 @@ if (Meteor.isClient) {
 
 		$(document).off('keypress', load_experiment);
 		$(document).on('keypress', present_next_card);
+		$(document).focus();
 	};
 
 	$(document).ready(function () {
