e8acef347ae304cceb7064e3458c7f9797fe2e4e
Matt Dahl <matt.dahl.2013@gmail.com>
2014-05-04 16:53:00 -0700

    Adds more statistical tests for other variables.

--- private/do_stats.R
+++ private/do_stats.R
@@ -5,27 +5,44 @@ library('rjson');
 
 data = fromJSON(file='data/cogsci_data_2014-05-03T20:08:18.157774.json');
 
-response_array = array(0, c(length(data)/24, 2), dimnames = list(NULL, c('good_chord_times', 'bad_chord_times')));
+response_times_per_user = array(0, c(length(data)/24, 2), dimnames = list(NULL, c('good_chord_times', 'bad_chord_times')));
+response_times_per_card = array(0, c(12, 2), dimnames = list(NULL, c('good_chord_times', 'bad_chord_times')));
+errors_per_user = array(0, c(length(data)/24, 2), dimnames = list(NULL, c('good_chord_errors', 'bad_chord_errors')));
+errors_per_card = array(0, c(12, 2), dimnames = list(NULL, c('good_chord_errors', 'bad_chord_errors')));
 
 for (i in data) {
   if (i[['chord']] == 'good') {
-    response_array[i[['user_id']] + 1, 1] = response_array[i[['user_id']] + 1, 1] + i[['response_time']];
+    response_times_per_user[i[['user_id']] + 1, 1] = response_times_per_user[i[['user_id']] + 1, 1] + i[['response_time']];
+    response_times_per_card[as.numeric(gsub("\\D", "", i[['card_id']])), 1] = response_times_per_card[as.numeric(gsub("\\D", "", i[['card_id']])), 1] + i[['response_time']];
+    
+    if (i[['is_correct']] == FALSE) {
+      errors_per_user[i[['user_id']] + 1, 1] = errors_per_user[i[['user_id']] + 1, 1] + 1;
+      errors_per_card[as.numeric(gsub("\\D", "", i[['card_id']])), 1] = errors_per_card[as.numeric(gsub("\\D", "", i[['card_id']])), 1] + 1;
+    }
   }
   else if (i[['chord']] == 'bad') {
-    response_array[i[['user_id']] + 1, 2] = response_array[i[['user_id']] + 1, 2] + i[['response_time']];
+    response_times_per_user[i[['user_id']] + 1, 2] = response_times_per_user[i[['user_id']] + 1, 2] + i[['response_time']];
+    response_times_per_card[as.numeric(gsub("\\D", "", i[['card_id']])), 2] = response_times_per_card[as.numeric(gsub("\\D", "", i[['card_id']])), 2] + i[['response_time']];
+    
+    if (i[['is_correct']] == FALSE) {
+      errors_per_user[i[['user_id']] + 1, 2] = errors_per_user[i[['user_id']] + 1, 2] + 1;
+      errors_per_card[as.numeric(gsub("\\D", "", i[['card_id']])), 2] = errors_per_card[as.numeric(gsub("\\D", "", i[['card_id']])), 2] + 1;
+    }
   }
 }
 
-response_array = response_array / 12; # Take average response times
+response_times_per_user = response_times_per_user / 12; # Take average response times for each user for ALL cards
+response_times_per_card = response_times_per_card / (length(data) / 24); # Take average times for each card for ALL users
 
-diffs = response_array[,2] - response_array[,1]; # bad_chord - good_chord response time response time
 
 # Generate a histogram of the data - should be normally distributed if the t-test is to hold at low sample sizes
-hist(diffs);
-
-# State the hypotheses
-print('H_0: M = 0');
-print('H_a: M > 0');
+hist(response_times_per_user[,2] - response_times_per_user[,1]); # bad_chord - good_chord
+hist(response_times_per_card[,2] - response_times_per_card[,1]); # bad_chord - good_chord
+hist(errors_per_user[,2] - errors_per_user[,1]);# bad_chord - good_chord
+hist(errors_per_card[,2] - errors_per_card[,1]); # bad_chord - good_chord
 
 # Perform a matched pairs analysis on the data
-t.test(response_array[,'bad_chord_times'], response_array[,'good_chord_times'], alt="two.sided", paired=TRUE);
+t.test(response_times_per_user[,'bad_chord_times'], response_times_per_user[,'good_chord_times'], alt="greater", paired=TRUE);
+t.test(response_times_per_card[,'bad_chord_times'], response_times_per_card[,'good_chord_times'], alt="greater", paired=TRUE);
+t.test(errors_per_user[,'bad_chord_errors'], errors_per_user[,'good_chord_errors'], alt="greater", paired=TRUE);
+t.test(errors_per_card[,'bad_chord_errors'], errors_per_card[,'good_chord_errors'], alt="greater", paired=TRUE);
